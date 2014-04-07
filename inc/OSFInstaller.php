ba594d5bf64adf4d1a95cf08ed3b3cd49427ec3f
Frederick Giasson <fred@fgiasson.com>
2014-04-07 14:11:07 -0400

    Update the version of the Solr schema to 1.3.2

--- inc/OSFInstaller.php
+++ inc/OSFInstaller.php
@@ -1029,7 +1029,7 @@
       }
       else
       {
-        $this->exec('cp -f '.$this->osf_web_services_folder.$this->osf_web_services_ns.'/framework/solr_schema_v1_3_1.xml /usr/share/solr/osf-web-services/solr/conf/schema.xml');
+        $this->exec('cp -f '.$this->osf_web_services_folder.$this->osf_web_services_ns.'/framework/solr_schema_v1_3_2.xml /usr/share/solr/osf-web-services/solr/conf/schema.xml');
         
         $this->cecho("Restarting Solr...\n", 'WHITE');
         $this->exec('/etc/init.d/solr stop');
